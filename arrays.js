272279780b97bfd1c316b25c0067273652c7ad64
Dan Kagan <dankagan7@gmail.com>
2019-03-05 00:11:08 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,45 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
+
+function addElementToBeginningOfArray(chocolateBars) {
+  chocolateBars = ["foo", ...chocolateBars]
+  return chocolateBars
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars) {
+  chocolateBars.unshift("foo")
+  return chocolateBars;
+}
+
+function addElementToEndOfArray(chocolateBars) {
+  chocolateBars = [...chocolateBars, "foo"]
+  return chocolateBars
+}
+
+function destructivelyAddElementToEndOfArray(chocolateBars) {
+  chocolateBars.push("foo")
+  return chocolateBars;
+}
+
+function accessElementInArray(chocolateBars, index ) {
+  return chocolateBars[2];
+}
+
+///
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift(1,2);
+}
+///
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
+
+///
+function destructivelyRemoveElementFromEndOfArray(array) {
+  var destroyArray = array.pop(-1);
+  return destroyArray;
+}
+///
+function removeElementFromEndArray(array) {
+  
+}
+
